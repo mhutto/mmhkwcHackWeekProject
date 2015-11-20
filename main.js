c64bd3611fecb667a52bf1bbbb021adceb31d1a2
Morgan <mmhkwc@mail.missouri.edu>
2015-11-20 00:35:07 -0600

    Added files

---
+++ main.js
@@ -0,0 +1,39 @@
+/*
+TweenMax.to(".bells1", 10, {
+y:600,
+rotation:360,
+scale:1.2
+});
+*/
+
+
+TweenMax.to(".lamp2", 0.5, {
+	scale:5
+});
+
+TweenMax.to(".lamp3", 0.5, {
+	delay:0.5,
+	scale:5
+});
+
+TweenMax.to(".tree4", 2, {
+	delay: 1,
+	scale:5,
+	y:-250
+});
+
+
+TweenMax.to(".sleigh5", 10, {
+	delay: 3,
+	transformPerspective:600,
+	y:-420,
+	x:2000
+	
+});
+
+
+
+TweenMax.to(".lights6", 0.3, {
+	scale:4.2,
+	x:1
+});
